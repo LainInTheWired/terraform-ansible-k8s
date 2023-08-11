abbed0e0c489e86807cd7254374cd816f8f7807f
yakitate Ja pan <flx503@outlook.jp>
2023-08-11 17:26:23 +0900

    first commit

---
+++ aws-ec2.tf
@@ -0,0 +1,42 @@
+#----------------------------------------
+# EC2インスタンスの作成
+#----------------------------------------
+resource "aws_instance" "appache" {
+  count = 1
+  ami                    = "ami-053b0d53c279acc90" # ubuntu22:04
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.sample_subnet.id
+  key_name               = aws_key_pair.key_pair.id
+  vpc_security_group_ids = [aws_security_group.sample_sg.id]
+  
+  user_data = <<EOF
+#! /bin/bash
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+EOF
+}
+#----------------------------------------
+# EC2インスタンスの作成
+#----------------------------------------
+resource "aws_instance" "sample_web_server" {
+  ami                    = "ami-053b0d53c279acc90" # ubuntu22:04
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.sample_subnet.id
+  key_name               = aws_key_pair.key_pair.id
+  vpc_security_group_ids = [aws_security_group.sample_sg.id]
+  
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(local.private_key_file)
+      host        = aws_instance.sample_web_server.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i ${aws_instance.sample_web_server.public_ip}, --private-key ${module.aws-keypair.private_key_file} nginx.yaml"
+  }
+}
