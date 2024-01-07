eae207f6942f25eb07b79279c596b9db7f82192f
yakitate Ja pan <flx503@outlook.jp>
2024-01-07 14:58:59 +0900

    pve

--- terraform/template/pve/provider.tf
+++ terraform/template/pve/provider.tf
@@ -7,9 +7,18 @@ terraform {
     }
   }
 }
+variable "tr_pve_url" {
+  type = string
+}
+variable "tr_pve_user" {
+  type = string
+}
+variable "tr_pve_password" {
+  type = string
+}
 # 设置 Proxmox Provider
 provider "proxmox" {
-  pm_api_url   = "https://10.0.1.30:8006/api2/json" # Proxmox API 地址
-  pm_user      = "root@pam" # Proxmox 用户名
-  pm_password  = "rinriN@PVE01"  
+  pm_api_url   = var.tr_pve_url # Proxmox API 地址
+  pm_user      = var.tr_pve_url # Proxmox 用户名
+  pm_password  = var.tr_pve_password  
 }
